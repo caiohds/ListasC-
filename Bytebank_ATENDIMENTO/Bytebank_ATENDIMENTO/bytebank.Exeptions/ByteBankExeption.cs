9a09ef6e0dd4525b87b8617dd70035b181199073
caiohds <caiohds2020@gmail.com>
2022-11-03 16:21:59 -0300

    Tratando exceção

--- Bytebank_ATENDIMENTO/Bytebank_ATENDIMENTO/bytebank.Exeptions/ByteBankExeption.cs
+++ Bytebank_ATENDIMENTO/Bytebank_ATENDIMENTO/bytebank.Exeptions/ByteBankExeption.cs
@@ -10,10 +10,10 @@ namespace bytebank_ATENDIMENTO.bytebank.Exeptions
 	[Serializable]
 	public class ByteBankExeption : Exception
 	{
-		public MyException() { }
-		public MyException(string message) : base(message) { }
-		public MyException(string message, Exception inner) : base(message, inner) { }
-		protected MyException(
+		public ByteBankExeption() { }
+		public ByteBankExeption(string message) : base("Aconteceu uma exceção -> " + message) { }
+		public ByteBankExeption(string message, Exception inner) : base(message, inner) { }
+		protected ByteBankExeption(
 		  System.Runtime.Serialization.SerializationInfo info,
 		  System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
 	}
